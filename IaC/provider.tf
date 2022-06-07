4e0fb1a35fc6f06ae64445d0230b3f543426d82d
Abzali8806 <Abdullahali8102@gmail.com>
2022-06-07 17:36:28 +0100

    tf proivder version modified

--- IaC/provider.tf
+++ IaC/provider.tf
@@ -4,15 +4,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      Version = "~>3.27"
+      version = "~>3.0"
     }
   }
-
-  required_version = ">=0.14.9"
-
 }
 
 provider "aws" {
-  version = "~>3.0"
   region  = "east-us-1"
 }
